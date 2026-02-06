0b80186d647ae043a09042ae704775848905c966
MJB9595 <wansja7870@gmail.com>
2026-02-06 10:39:27 +0900

    끝말잇기 게임

---
+++ src/components/WordGame.jsx
@@ -0,0 +1,59 @@
+import React, {useState, useEffect, useRef} from 'react'
+import './WordGame.scss'
+
+const WordGame = ({startWord}) => {
+    const [word, setWord]=useState(startWord)
+    const [userInput, setUserInput]=useState("")
+    const [message, setMessage]=useState("끝말잇기 시작!")
+    const InputRef=useRef(null)
+
+    useEffect(()=> {
+        InputRef.current.focus()
+    },[])
+
+    const handleChange=(e)=>{
+        setUserInput(e.target.value)
+
+    }
+
+    const checkWord=()=>{
+        const trimmdWord = userInput.trim()
+
+        if (!trimmdWord) {
+            setMessage("단어를 입력하세요")
+            return
+        }
+        const lastChar = word[word.length-1]
+        const firstChar = trimmdWord[0]
+        // console.log(lastChar, firstChar)
+
+        if (lastChar !== firstChar) {
+            setMessage(`'${lastChar}' 로 시작하는 단어를 입력하세요`)
+            setUserInput("")
+        }else{
+            setMessage("성공! 다음 단어를 입력하세요!")
+            setWord(trimmdWord)
+        }
+        setUserInput("")
+    }
+    return (
+        <div className='game-container'>
+            <h1>끝말잇기</h1>
+            <p className='current-word'>
+                {word}
+            </p>
+
+            <input 
+            type="text" 
+            ref={InputRef}
+            onChange={handleChange}
+            onKeyUp={(e)=>e.key==='Enter' && checkWord()}
+            value={userInput}/>
+
+            <button onClick={checkWord}>확인</button>
+            <p className='message'>{message}</p>
+        </div>
+    )
+    }
+
+export default WordGame
